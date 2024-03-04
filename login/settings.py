31092783911b0806ea92e6ac43ede1aa3d23bb67
Gabriel-l-c <gabriellyra.c@gmail.com>
2024-03-04 14:55:48 -0300

    vercel deploy

--- login/settings.py
+++ login/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = "django-insecure-tqymyk0log35gn8bvw9cjiv9hi9fw&&w50$xdk)#b(g1t4f97)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
